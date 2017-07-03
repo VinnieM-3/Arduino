c1d19aefa3bd52604a27ee78d2b8ea053d8a1315
VinnieM <vm_hobbies@yahoo.com>
2017-07-02 22:01:21 -0400

    Update WDT_Simple.ino

--- WDT_Simple.ino
+++ WDT_Simple.ino
@@ -1,7 +1,9 @@
-// This sketch demonstrates the Watchdog Timer in both Interrupt Mode
-// and System Reset Mode.
+/*
+ * This sketch demonstrates the Watchdog Timer 
+ * along with the Interrupt and System Reset modes.
+ */
 
-#include <avr/wdt.h> 
+#include <avr/wdt.h>
 
 unsigned long x=0;
 
@@ -13,30 +15,55 @@ void setup()
   cli();
   
   // Reset Watchdog Timer
-  MCUSR &= ~(1<<WDRF);  
+  MCUSR &= ~(1<<WDRF);
   
   // Prepare watchdog for config change.
   WDTCSR |= (1<<WDCE) | (1<<WDE);
   
-  // Now choose timer mode and prescaler
-  WDTCSR = (1<<WDIE) | (1<<WDP3);  // 4sec, Interrupt Mode
-  //WDTCSR = (1<<WDE) | (1<<WDP3);  // 4sec, System Reset Mode
-  //WDTCSR = (1<<WDIE) | (1<<WDP2) | (1<<WDP1) | (1<<WDP0);  // 2sec, Interrupt Mode
+  /* Now choose a timer mode and prescaler from the choices below:
+  
+      WDTCSR Register Notes:
+
+      WDE WDIE Mode
+       0   1   Interrupt Mode Interrupt
+       1   0   System Reset Mode Reset
+       1   1   Interrupt and System Reset
+  
+      WDP3 WDP2 WDP1 WDP0
+       0    0    0    0     16 msec
+       0    0    0    1     32 msec
+       0    0    1    0     64 msec
+       0    0    1    1    125 msec
+       0    1    0    0    250 msec
+       0    1    0    1    500 msec
+       0    1    1    0      1 sec
+       0    1    1    1      2 sec
+       1    0    0    0      4 sec
+       1    0    0    1      8 sec
+ */
+
+  // Uncomment one of these examples or create your own:
+  //WDTCSR = (1<<WDIE) | (1<<WDP2) | (1<<WDP1) | (1<<WDP0); // 2sec, Interrupt Mode
+  WDTCSR = (1<<WDIE) | (1<<WDP3);                         // 4sec, Interrupt Mode 
   //WDTCSR = (1<<WDE) | (1<<WDP2) | (1<<WDP1) | (1<<WDP0);  // 2sec, System Reset Mode 
-   
+  //WDTCSR = (1<<WDE) | (1<<WDP3);                          // 4sec, System Reset Mode
+
+ 
   // Enable interrupts globally
-  sei();	
-} 
+  sei();
+}
 
 void loop()
 {
+  /* Resetting the Watchdog (i.e. running wdt_reset()) before the Watchdog timer 
+   * expires will prevent the interrupt and/or system reset. Commenting out 
+   * wdt_reset() will cause either an interrupt and/or system reset. A system reset 
+   * will cause x to start back at 1, while an interrupt will simply display the 
+   * "INTERRUPT!!!" message and continue the x loop where it left off.
+   */
+   
   delay(1000);
-  Serial.println(++x); 
-  
-  // Resetting the timer in less than 4sec will prevent the interrupt or system reset.
-  // Commenting out the reset will cause either an interrupt or system reset.
-  // System reset will cause x to start back at 1, while an interrupt will
-  // simply display the "INTERRUPT!!!" message and continue the loop where it left off.
+  Serial.println(++x); //print incrementer value
   //wdt_reset();
 }
 
