d7e8b682a76e18be0bf3d327ef7f4c994c2cc01d
VinnieM <vm_hobbies@yahoo.com>
2017-07-04 12:26:01 -0400

    Update Timer_4secIntr.ino

--- Timer_4secIntr.ino
+++ Timer_4secIntr.ino
@@ -2,7 +2,22 @@
  * Timer1 can trigger an interrupt every ~4sec or less 
  * The LED tied to Arduino pin 13 will toggle every time the 
  * interrupt is triggered.
- */
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+*/
 
 void setup(){
   
