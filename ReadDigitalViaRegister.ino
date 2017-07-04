bbe983cfebb8ce180603770693d49c7d87a4ec34
VinnieM <vm_hobbies@yahoo.com>
2017-07-04 11:56:56 -0400

    Update ReadDigitalViaRegister.ino

--- ReadDigitalViaRegister.ino
+++ ReadDigitalViaRegister.ino
@@ -1,27 +1,33 @@
 // This sketch demonstrates reading a digital input using registers.
 // Arduino pin 7 will be read and the LED on pin 13 will mirror pin 7's state.
 // Either connect pin 7 to ground (with a current limiting resistor) 
-// or leave it open and the pullup will pull it high.
-
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif 
+// or leave it open and the internal pullup will pull it high.
 
 void setup() {
   
   // Set Arduino pin 7 as an input and turn on the pullup resistor.
-  cbi(DDRD, DDD7); // pin 7 is an input
-  sbi(PORTD,PORTD7); // pullup turned on
+  //DDRD &= ~(1<<DDD7);   // set pin 7 as an input
+  //PORTD |= (1<<PORTD7); // activate internal pullup resistor
+  pinMode(7, INPUT_PULLUP); // or set pin 7 using the Arduino function
   
   // Set Arduino pin 13 as an output
-  sbi(DDRB, DDB5);  
+  //DDRB |= (1<<DDB5);
+  pinMode(LED_BUILTIN, OUTPUT); // or set the using the Arduino function
 }
 
 void loop() {  
   // Read the value on pin 7 and set pin 13 accordingly.
-  //if ((1<<PORTD7) & PIND) sbi(PORTB,PORTB5); else cbi(PORTB, PORTB5); // if-else all on one line 
-  ((1<<PORTD7) & PIND) ? sbi(PORTB,PORTB5) : cbi(PORTB, PORTB5);  // or use a ternary operator 
+
+  // simple if-else statement
+  if ((1<<PORTD7) & PIND) {
+    PORTB |= (1<<PORTB5); 
+  } else {
+    PORTB &= ~(1<<PORTB5);
+  }
+
+  // or put the if-else all on one line
+  //if ((1<<PORTD7) & PIND) PORTB |= (1<<PORTB5); else PORTB &= ~(1<<PORTB5);
+
+  // or you could use a ternary operator 
+  //((1<<PORTD7) & PIND) ? PORTB |= (1<<PORTB5) : PORTB &= ~(1<<PORTB5);
 }
