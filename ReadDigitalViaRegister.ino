1e116e30d8f60a636d580f65f3b9aa1f59b41dc4
VinnieM <vm_hobbies@yahoo.com>
2017-07-04 12:22:26 -0400

    Update ReadDigitalViaRegister.ino

--- ReadDigitalViaRegister.ino
+++ ReadDigitalViaRegister.ino
@@ -1,7 +1,24 @@
-// This sketch demonstrates reading a digital input using registers.
-// Arduino pin 7 will be read and the LED on pin 13 will mirror pin 7's state.
-// Either connect pin 7 to ground (with a current limiting resistor) 
-// or leave it open and the internal pullup will pull it high.
+/* This sketch demonstrates reading a digital input using registers.
+ * Arduino pin 7 will be read and the LED on pin 13 will mirror pin 7's state.
+ * Either connect pin 7 to ground using a current limiting resistor
+ * or leave it open and the internal pullup will pull it high.
+ *  
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+*/
+
 
 void setup() {
   
